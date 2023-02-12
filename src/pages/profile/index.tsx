b6f6174d684746075cb4c1e7acf3d7786771e493
tohzt <steven.g.french@live.com>
2023-02-11 21:18:47 -0700

    basic cleanup

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,9 +6,17 @@ import ProfileIcon from "../../components/ProfileIcon";
 const ProfilePage = () => {
   const { data: session } = useSession();
 
-  return ( session &&
+  return (session &&
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="pt-10 text-white">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            The <span className="text-[hsl(280,100%,70%)]">Profile</span>
+          </h1>
+          <p className="text-2xl">
+            Basic Profile Shit
+          </p>
+        </div>
         <ProfileIcon />
         <span> User:    </span>
         <span>{session?.user?.name}</span>
