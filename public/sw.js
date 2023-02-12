fa6770bdd5dc8aa8ce86d5b8fee3e66a0f4def88
tohzt <steven.g.french@live.com>
2023-02-12 13:50:24 -0700

    pwa seems to be working

---
+++ public/sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "shed-cache";
+const urlsToCache = [
+  "index.html",
+  "offline.html",
+  // "Kerwood40.glb",
+  // "Frank.glb",
+  // "Andrew.glb",
+  // "SceneBackground.glb",
+];
+const self = this;
+
+//Install Service Worker
+self.addEventListener("install", (event) => {
+  console.log("Hello from service worker!")
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened Cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => console.log("Didn't Add Cache", err))
+  );
+});
+//Listen for Requests
+self.addEventListener("fetch", (event) => {
+  console.log("fetching");
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      console.log("event", event.request);
+      console.log("cacheRes", cacheResponse);
+      return (
+        cacheResponse ||
+        fetch(event.request).catch(() => caches.match("offline.html"))
+      );
+    })
+  );
+});
+
+//Activate the Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  clients.claim();
+  console.log('Ready!');
+});
+
+// console log something when this service worker is registered
+console.log('Service Worker Registered');
