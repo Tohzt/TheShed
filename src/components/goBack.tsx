2039bbd5cc0f3363d2720885fe523a18661d6d92
tohzt <steven.g.french@live.com>
2023-02-14 22:50:35 -0700

    some bugs

--- src/components/goBack.tsx
+++ src/components/goBack.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 
 const GoBack: React.FC = () => {
-  let placement = "absolute -bottom-[20vw] items-center justify-center rounded-full left-[0vw] -bottom-[20vw] ";
+  const placement = "absolute -bottom-[20vw] items-center justify-center rounded-full left-[0vw] -bottom-[20vw] ";
   return (
     <div className="">
       <div className={`${placement}w-[42vw] h-[42vw] translate-y-2 translate-x-2 bg-zinc-900 bg-opacity-80 blur-sm `}></div>
