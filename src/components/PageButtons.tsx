26268584f3da3bf22c7cf740266a8100181e9399
tohzt <steven.g.french@live.com>
2023-02-14 22:30:55 -0700

    added mario game to arcade

--- src/components/PageButtons.tsx
+++ src/components/PageButtons.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 
-const PageButtons: React.FC<{pagepath: string; label: string;}> = (props) => {
+const PageButtons: React.FC<{style: string; pagepath: string; label: string;}> = (props) => {
+  let style = "w-[100px] h-[100px] flex justify-center items-center rounded-lg bg-blue-600 " + props?.style;
   return (
     <Link href={props?.pagepath}>
-      <div className="w-[100px] h-[100px] flex justify-center items-center rounded-lg bg-blue-600">
+      <div className={style}>
         {props?.label}
       </div>
     </Link>
