06adbeec5060194cf1938933a657fdbc1f08bb9b
tohzt <steven.g.french@live.com>
2023-02-17 00:47:35 -0700

    hella revamp

--- src/components/goBack.tsx
+++ src/components/goBack.tsx
@@ -1,18 +1,11 @@
 import Link from "next/link";
 
-const GoBack: React.FC = () => {
-  const placement = "h-[4vh] absolute bottom-0 w-screen items-center justify-center rounded-t-full "
-  return (
-    <div className={placement}>
-      <div className={`${placement}bg-zinc-900 bg-opacity-60 blur-sm -translate-y-2`}></div>
-      <div className={`${placement}bg-zinc-600`}>
-        <Link href="/">
-          <div className="flex justify-center text-[hsl(280,100%,70%)]">
-            Go Back
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
-};
-export default GoBack;
+const goBack = (
+  <div className="flex items-center justify-center w-full h-full font-semibold text-slate-800">
+    <Link href="/">
+      Go Back
+    </Link>
+  </div>
+)
+
+export default goBack
--- src/pages/arcade/index.tsx
+++ src/pages/arcade/index.tsx
@@ -1,31 +1,25 @@
 import * as React from "react"
 import { useSession } from "next-auth/react";
-import ProfileIcon from "../../components/profileIcon";
-import GoBack from "../../components/goBack";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import goBack from "../../components/goBack";
 
 const ArcadePage = () => {
   const { data: session } = useSession();
 
   return (session &&
     <>
-      <ProfileIcon />
-      <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="pt-10 text-white">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            The <span className="text-[hsl(280,100%,70%)]">Arcade</span>
-          </h1>
-          <p className="text-2xl">
-            Basic Profile Shit
-          </p>
-        </div>
+      <Header title="Arcade" description="Touch My Buttons" />
 
+      <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="flex flex-col flex-1 justify-around">
-        <div className="bg-zinc-900 w-[90vw] h-[135vw] border-4 border-zinc-400 flex items-center justify-center">
-          <iframe title='Mario Bros' src="../../../arcade/mario/index.html" width='100%' height='100%'></iframe>
-        </div>
+          <div className="bg-zinc-900 w-[90vw] h-[135vw] max-h-[80vh] border-4 border-zinc-400 flex items-center justify-center">
+            <iframe title='Mario Bros' src="../../../arcade/mario/index.html" width='100%' height='100%'></iframe>
+          </div>
         </div>
       </div>
-      <GoBack />
+
+      <Footer func={goBack} />
     </>
   );
 };
