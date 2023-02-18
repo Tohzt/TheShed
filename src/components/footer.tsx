4fe745578600a569c69ecde1c7f8e10a1587ac3f
tohzt <steven.g.french@live.com>
2023-02-17 19:39:31 -0700

    fixed a render loop bug

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer: React.FC<{ func: any; size: string }> = (props) => {
   return (
     <div className={placement}>
       <div className={`${placement} h-[5vh] bg-zinc-900 bg-opacity-60 blur-sm`}></div>
-      <div className={`${placement} ${props.size} bg-gradient-to-tl from-[#ef8018] to-[#ffcb24] flex flex-col`}>
+      <div className={`${placement} ${props.size} transition-height duration-500 ease-in-out bg-gradient-to-tl from-[#ef8018] to-[#ffcb24] flex flex-col`}>
         {props.func}
       </div>
     </div>
