b4ea9dcbf5892d0134a53ba6a393559f5de86c2a
tohzt <steven.g.french@live.com>
2023-02-17 20:16:22 -0700

    disable selecting game

--- src/pages/arcade/index.tsx
+++ src/pages/arcade/index.tsx
@@ -13,7 +13,7 @@ const ArcadePage = () => {
 
       <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="flex flex-col flex-1 justify-around">
-          <div className="bg-zinc-900 w-[90vw] h-[135vw] max-h-[80vh] border-4 border-zinc-400 flex items-center justify-center">
+          <div className="select-none bg-zinc-900 w-[90vw] h-[135vw] max-h-[80vh] border-4 border-zinc-400 flex items-center justify-center">
             <iframe title='Mario Bros' src="../../../arcade/mario/index.html" width='100%' height='100%'></iframe>
           </div>
         </div>
