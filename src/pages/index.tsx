d7baa712d6c0d5c27ca53806cc7b8410e6471ec5
tohzt <steven.g.french@live.com>
2023-02-18 19:58:39 -0700

    working.. but not quite how i want

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,59 +2,12 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import PageButtons from "../components/PageButtons";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { type GetServerSideProps } from "next";
+import { type AppProps } from "next/app";
+import { getProviders, useSession } from "next-auth/react";
 
-const Home: React.FC = () => {
+const Home = ({ providers }: { providers: AppProps }) => {
   const { data: session } = useSession();
-  const [isOpen, setIsOpen] = useState(false);
-  const [footerHeight, setFooterHeight] = useState("h-[4vh] ");
-
-  useEffect(() => {
-    console.log("change footer")
-    if (isOpen) 
-      setFooterHeight("h-[20vh] ")
-    else 
-      setFooterHeight("h-[4vh] ")
-  }, [isOpen]);
-
-  const bounce = () => {
-    setFooterHeight("h-[6vh] ")
-    setTimeout(() => {
-      setFooterHeight("h-[4vh] ")
-    }, 200);
-  }
-
-  const signInOut = (
-    <>
-      {session ? (
-        <>
-          <div className="flex flex-1 flex-col h-full w-full justify-center items-center font-semibold text-slate-800">
-            <button onClick={() => void signOut()}>
-              Log Out
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <div
-            className="h-[4vh] w-full flex justify-center font-semibold text-slate-800"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            {isOpen ? "Pick One" : "Log In"}
-          </div>
-          {isOpen &&
-            <div className="flex flex-1 flex-col h-full w-full justify-center items-center">
-              <div className="flex items-center justify-center w-[25vw] h-[20vw] rounded-full font-semibold bg-blue-400 text-slate-300 border-4 border-slate-300">
-                <button onClick={() => void signIn()}>
-                  Discord
-                </button>
-              </div>
-            </div>
-          }
-        </>
-      )}
-    </>
-  )
 
   return (
     <>
@@ -67,21 +20,12 @@ const Home: React.FC = () => {
               <>
                 <PageButtons pagepath="/profile" label="PROFILE" style="" />
                 <PageButtons pagepath="/arcade" label="ARCADE" style="" />
-                {/*
-                <PageButtons pagepath="/profile" label="CALENDAR" style="" />
-                <PageButtons pagepath="/profile" label="ALARM" style="" />
-                <PageButtons pagepath="/profile" label="BIDDING" style="" />
-                <PageButtons pagepath="/profile" label="PROJECTS" style="" />
-                <PageButtons pagepath="/profile" label="..." style="" />
-                <PageButtons pagepath="/profile" label="..." style="" />
-                <PageButtons pagepath="/profile" label="..." style="" />
-                */}
               </>
             ) : (
               <>
+                <PageButtons pagepath="/arcade" label="ARCADE" style="" />
                 <div
                   className="w-[20vw] h-[20vw] rounded-lg bg-white"
-                  onClick={() => {bounce()}}
                 >
                   test Bounce
                 </div>
@@ -90,10 +34,17 @@ const Home: React.FC = () => {
           </div>
         </div>
 
-        <Footer func={signInOut} size={footerHeight} />
+        <Footer providers={providers} open="Nevermind" closed={session ? "Log Out" : "Log In"} />
       </main>
     </>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
