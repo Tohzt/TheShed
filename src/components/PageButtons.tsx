f49c004cedf3be786f8fa32d1d6c56c2f93fbcf2
tohzt <steven.g.french@live.com>
2023-02-20 23:19:11 -0700

    Got zustand working

--- src/components/PageButtons.tsx
+++ src/components/PageButtons.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 const PageButtons: React.FC<{style: string; pagepath: string; label: string;}> = (props) => {
-  const style = "w-[100px] h-[100px] flex justify-center items-center rounded-lg bg-blue-600 " + props?.style;
+  const style = "w-full h-[100px] flex justify-center items-center rounded-lg bg-blue-600 " + props?.style;
   return (
     <Link href={props?.pagepath}>
       <div className={style}>
