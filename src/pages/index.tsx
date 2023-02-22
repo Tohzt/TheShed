e8c49ec462e045679057377f74eeff0cbdeca945
tohzt <steven.g.french@live.com>
2023-02-22 00:28:54 -0700

    rearanged signout/footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from "next/head";
 import React from "react";
 import PageButtons from "../components/PageButtons"
 import Header from "../components/header"
-import Footer from "../components/Footer"
+//import Footer from "../components/Footer"
+import SignInOut from "../components/signInOut"
 //import { useStore } from "../../store/store"
 
 // an object that holds multiple PageButtons componnents
@@ -14,17 +15,16 @@ interface pagesType {
   }
 }
 const pages: pagesType = {
-  "home": { pagePath: "/", label: "HOME", style: "bg-primary" },
-  "about": { pagePath: "/", label: "ABOUT", style: "bg-primary" },
-  "arcade": { pagePath: "/arcade", label: "ARCADE", style: "" },
   "profile": { pagePath: "/profile", label: "PROFILE", style: "" },
-  "calendar": { pagePath: "/", label: "CALENDAR", style: "bg-primary" },
   "alarm": { pagePath: "/", label: "ALARM", style: "bg-primary" },
+  "calendar": { pagePath: "/", label: "CALENDAR", style: "bg-primary" },
+  "arcade": { pagePath: "/arcade", label: "ARCADE", style: "" },
+  "about": { pagePath: "/", label: "ABOUT", style: "bg-primary" },
 }
 
 const displayPages = () => {
   return Object.keys(pages).map((page, index) => {
-    const style = pages[page]?.style + (index%2 === 0 ? " offset-left" : " offset-right")
+    const style = pages[page]?.style + (index % 2 === 0 ? " offset-left" : " offset-right")
     return <PageButtons key={index} pagePath={pages[page]?.pagePath} label={pages[page]?.label} style={style} />
   })
 }
@@ -45,18 +45,20 @@ const Home: React.FC = () => {
       <main className="fixed bg-gradient-to-t from-primary-light to-primary-dark">
         <Header />
 
-        <div className="screen flex-col -center">
-          <div className="w-full overflow-y-auto flex-col gap-4">
-                {/*
-                  <button onClick={() => { toggleDarkMode(!darkMode) }}>
-                    hello
-                  </button>
-                */}
-              {displayPages()}
+        <div className="screen flex-col -center justify-start">
+          <div className="pt-[55vw] w-full overflow-y-auto flex-col gap-4">
+            {displayPages()}
+            {/*
+              <button onClick={() => { toggleDarkMode(!darkMode) }}>
+                hello
+              </button>
+            */}
           </div>
         </div>
 
-        <Footer />
+        <div className="abs-br w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tl-2xl border-l-4 border-t-2 border-white">
+          <SignInOut />
+        </div>
       </main>
     </>
   );
