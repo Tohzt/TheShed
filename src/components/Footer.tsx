de4255804b5fcefced959b3fcbd4dcdddf6e025b
tohzt <steven.g.french@live.com>
2023-02-22 21:02:21 -0700

    separated log in/out  and brought over to profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import SignInOut from "./signInOut"
+import SignIn from "./signIn"
+import SignOut from "./signOut"
+import GoBack from "./goBack";
+import { useSession } from "next-auth/react"
 
-const Footer = () => {
+interface FooterProps {
+  goBack: boolean;
+  signIn: boolean;
+  signOut: boolean;
+}
+
+const Footer = (props: FooterProps) => {
+  const { data: sessionData } = useSession();
 
   return (
-    <div className="abs-br w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tl-2xl border-l-4 border-t-2 border-white">
-      <SignInOut />
+    <div>
+      {props.signIn && !sessionData && 
+        <div className="abs-br w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tl-2xl border-l-4 border-t-2 border-white">
+          <SignIn />
+        </div>
+      }
+      {props.signOut && sessionData && 
+        <div className="abs-br w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tl-2xl border-l-4 border-t-2 border-white">
+          <SignOut />
+        </div>
+      }
+      {props.goBack &&
+        <div className="abs-bl w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tr-2xl border-r-4 border-t-2 border-white">
+          <GoBack />
+        </div>
+      }
     </div>
   );
 };
--- src/components/goBack.tsx
+++ src/components/goBack.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const GoBack: React.FC = () => {
   return (
-    <div className="flex -center abs-bl w-[25vw] h-[25vw] overflow-hidden bg-secondary-dark rounded-tr-2xl border-r-4 border-t-2 border-white">
-      <div className="flex -center w-[20vw] h-[20vw] bg-primary-light text-white rounded-2xl border-2 border-white">
-        <Link href="/">Back</Link>
-      </div>
-    </div>
+    <Link
+      className="flex -center w-[20vw] h-[20vw] bg-primary-light text-white rounded-2xl border-2 border-white"
+      href="/">
+      Back
+    </Link>
   );
 };
 export default GoBack
