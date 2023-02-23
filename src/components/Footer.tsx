a6d452745dbb0f512e36ea9bdb188ddb78e562ff
tohzt <steven.g.french@live.com>
2023-02-22 18:50:42 -0700

    extractud sign in and updated arcade

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import SignInOut from "./signInOut"
 const Footer = () => {
 
   return (
-    <div className="abs-bot w-screen flex-col -center bg-primary rounded-t-2xl">
+    <div className="abs-br w-[25vw] h-[25vw] overflow-hidden flex -center bg-secondary-dark rounded-tl-2xl border-l-4 border-t-2 border-white">
       <SignInOut />
     </div>
   );
--- src/pages/arcade/index.tsx
+++ src/pages/arcade/index.tsx
@@ -2,26 +2,20 @@ import * as React from "react"
 //import { useSession } from "next-auth/react";
 import ProfileIcon from "../../components/ProfileIcon";
 import GoBack from "../../components/goBack";
+import Header from "../../components/header"
+import Link from 'next/link';
 
 const ArcadePage = () => {
   //const { data: session } = useSession();
 
   return (true &&
     <>
-      <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="pt-10 text-white">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            The <span className="text-[hsl(280,100%,70%)]">Arcade</span>
-          </h1>
-          <p className="text-2xl">
-            Basic Profile Shit
-          </p>
-        </div>
-        <ProfileIcon />
-        <br />
-
-        <div className="bg-zinc-900 w-[60vw] h-[80vw] border-4 border-zinc-400 flex items-center justify-center">
-          <iframe title='Mario Bros' src="../../../arcade/mario/index.html" width='240px' height='360px'></iframe>
+      <Header />
+      <div className="screen bg-zinc-900 border-4 border-zinc-400 flex -center">
+        <div className="flex -center -column bg-secondary w-[20vw] h-[20vw] rounded-2xl border-4 border-white">
+          <Link href="/arcade/mario">
+            Mario
+          </ Link>
         </div>
       </div>
       <GoBack />
