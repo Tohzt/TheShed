4fcc6a7013a5794dc5368a1842c19b730779b1f2
tohzt <steven.g.french@live.com>
2023-06-30 12:54:37 -0700

    website availability primatives

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,9 +7,22 @@ const Header = () => {
   const { data: session } = useSession();
   const user = session ? session.user : null
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [width, setWidth] = React.useState<number>(900);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  React.useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  const isMobile = width <= 768;
 
   return (
-    <div className="abs-top w-screen max-h-[50vw] flex -center">
+    <div className="">
       <div className="header-profile-container"></div>
       <div className="header-profile-square"></div>
       <div className="header-profile-gap"></div>
@@ -17,20 +30,24 @@ const Header = () => {
       <div className="header-icon-inner"></div>
       <ProfileIcon />
       <div className="header-profile-rectangle">
-      <div className=" header w-full">
-        <div className="overflow-hidden flex -center h-[25vw] text-white text-lg font-heading">
-          {user ? (
-            <div>
-              <span>{user.name}</span>
+        <div className="w-full">
+          <div className="header-text">
+            <div className="flex-col -center gap-1">
+              {user && isMobile ? (
+                <div>
+                  <span>{user.name}</span>
+                </div>
+              ) : (
+                <>
+                  <span className="text-[3em] font-bold text-white">The</span>
+                  <span className="-translate-x-2 text-[3em] font-bold text-white">Shed</span>
+                  {/*
+                {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+                */}
+                </>
+              )}
             </div>
-          ) : (
-              <div className="flex-col -center gap-1">
-                <span className="text-[3em] font-bold text-white">The</span>
-                <span className="-translate-x-2 text-[3em] font-bold text-white">Shed</span>
-                {/*hello.data ? hello.data.greeting : "Loading tRPC query..."}*/}
-              </div>
-          )}
-        </div>
+          </div>
         </div>
       </div>
     </div>
