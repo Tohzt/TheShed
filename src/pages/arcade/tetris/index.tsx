00b06e6fdfcc16bd32b6e91132767ce288cc0413
tohzt <steven.g.french@live.com>
2023-07-14 20:46:04 -0700

    attempting to add Godot 4 game

---
+++ src/pages/arcade/tetris/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const Godot = () => {
+  return (
+      <div className="screen bg-slate-900" >
+        <iframe className="unselectable" title='TETRIS' src="../../../arcade/tetris/Tetris.html" width='1080px' height='2400px'></iframe>
+      </div >
+  )
+};
+
+export default Godot
+
