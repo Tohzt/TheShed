0f22d14238d000f27edc9bad141ecc1c7c0bf77e
Tohzt <steven.g.french@live.com>
2023-07-25 16:56:06 -0700

    added tetris back and changed frame size via code

--- src/pages/arcade/tetris/index.tsx
+++ src/pages/arcade/tetris/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const Godot = () => {
   return (
       <div className="screen bg-slate-900" >
-        <iframe className="unselectable" title='TETRIS' src="../../../arcade/tetris/index.html" width='1080px' height='2400px'></iframe>
+        <iframe className="unselectable" title='TETRIS' src="../../../arcade/tetris/index.html" width='256px' height='320px'></iframe>
       </div >
   )
 };
