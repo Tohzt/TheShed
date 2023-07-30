36d0f73a55ea95ff75cf498fa26506d4ca959057
Tohzt <steven.g.french@live.com>
2023-07-30 14:24:30 -0700

    updated PageButton component with GPTs help

--- src/pages/arcade/godot/index.tsx
+++ src/pages/arcade/godot/index.tsx
@@ -2,9 +2,9 @@ import * as React from "react"
 
 const Godot = () => {
   return (
-      <div className="screen bg-slate-900" >
-        <iframe className="unselectable" title='Mario Bros' src="../../../arcade/godot/Survivor.html" width='1080px' height='2400px'></iframe>
-      </div >
+    <div className="screen bg-slate-900" >
+      <iframe className="unselectable w-screen h-screen" title='Mario Bros' src="../../../arcade/godot/Survivor.html"></iframe>
+    </div >
   )
 };
 
--- src/pages/arcade/tetris/index.tsx
+++ src/pages/arcade/tetris/index.tsx
@@ -2,9 +2,9 @@ import * as React from "react"
 
 const Godot = () => {
   return (
-      <div className="screen bg-slate-900" >
-        <iframe className="unselectable w-screen h-screen" title='TETRIS' src="../../../arcade/tetris/index.html"></iframe>
-      </div >
+    <div className="screen bg-slate-900" >
+      <iframe className="unselectable w-screen h-screen" title='TETRIS' src="../../../arcade/tetris/index.html"></iframe>
+    </div >
   )
 };
 
