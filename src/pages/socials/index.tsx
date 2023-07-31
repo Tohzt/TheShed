68ea4b98b8d5827aeae2b99938161dbe18b623f0
Tohzt <steven.g.french@live.com>
2023-07-30 18:46:07 -0700

    added social links to social page

--- src/pages/socials/index.tsx
+++ src/pages/socials/index.tsx
@@ -1,11 +1,48 @@
 import * as React from "react"
-//import { useSession } from "next-auth/react";
 import Header from "../../components/header"
 import Footer from "../../components/Footer"
+import SocialButtons from "../../components/SocialButtons"
+
+interface socialType {
+  [key: string]: {
+    path: string,
+    style: string
+  }
+}
+const socials: socialType = {
+  "Personal Instagram": {
+    path: "https://www.instagram.com/im_just.a.me",
+    style: ""
+  },
+  "Tattoo Instagram": {
+    path: "https://www.instagram.com/tat.tohzt",
+    style: "bg-primary"
+  },
+  "Personal YouTube": {
+    path: "https://www.youtube.com/c/godsautobiography",
+    style: ""
+  },
+  "LetsClone YouTube": {
+    path: "https://www.youtube.com/c/letsclone",
+    style: ""
+  },
+}
+
+const displaySocials = () => {
+  return Object.keys(socials).map((page, index) => {
+    const style = socials[page]?.style
+    return (
+      <SocialButtons
+        key={index}
+        path={socials[page]?.path}
+        label={page}
+        style={style}
+      />
+    )
+  })
+}
 
 const SocialsPage = () => {
-  //const { data: session } = useSession();
-
   return (true &&
     <>
       <Header />
@@ -14,18 +51,28 @@ const SocialsPage = () => {
         <div className="header-margin"></div>
         <div
           className="
-          flex flex-1
+            flex 
             overflow-hidden
             max-h-[60vh]
-            w-[80vw]
+            w-[90vw]
             p-4 mt-[25vh]
             bg-secondary-dark 
             border-4 border-white 
             rounded-2xl 
             text-white text-center
           ">
-          <div className="h-full w-full bg-secondary-light overflow-auto rounded-lg p-2">
-            <h1>Socials</h1>
+          <div 
+            className="
+              bg-secondary-light 
+              flex-col -center 
+              gap-2
+              h-full w-full 
+              overflow-auto 
+              rounded-lg p-2
+              pb-8
+            ">
+            <h1 className="mb-8">Socials</h1>
+            {displaySocials()}
           </div>
         </div>
         <div className="footer-margin"></div>
