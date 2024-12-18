2593b63557f00e6a99fb1cde111d6c69bbffbf3f
Tohzt <steven.g.french@live.com>
2024-12-18 01:34:29 -0700

    see if it works now

--- next.config.js
+++ next.config.js
@@ -27,6 +27,31 @@ module.exports = {
         ],
       },
       {
+        source: '/arcade/huebound/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin'
+          },
+          {
+            key: 'Cross-Origin-Isolation',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ],
+      },
+      {
         source: '/arcade/tetris/(.*)',
         headers: [
           {
