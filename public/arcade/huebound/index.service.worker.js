570459d0eb2100367e66986191b1e08d792da6fb
Tohzt <steven.g.french@live.com>
2025-02-11 00:28:03 -0700

    updated huebound to v0.8

--- public/arcade/huebound/index.service.worker.js
+++ public/arcade/huebound/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738193589|63017887';
+const CACHE_VERSION = '1739258321|19675550035';
 /** @type {string} */
 const CACHE_PREFIX = 'HueBound-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
