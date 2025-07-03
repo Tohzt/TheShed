6eae8d649df84cf716d82178dbdbf9a227727970
Tohzt <steven.g.french@live.com>
2025-07-03 13:19:07 -0600

    updated socials page

--- src/pages/socials/index.tsx
+++ src/pages/socials/index.tsx
@@ -1,87 +1,74 @@
-import * as React from "react"
-import Header from "../../components/header"
-import Footer from "../../components/Footer"
-import SocialButtons from "../../components/SocialButtons"
+import * as React from 'react'
+import Header from '../../components/header'
+import Footer from '../../components/Footer'
+import SocialButtons from '../../components/SocialButtons'
 
 interface socialType {
-  [key: string]: {
-    path: string,
-    style: string
-  }
+	[key: string]: {
+		path: string
+		style: string
+	}
 }
 const socials: socialType = {
-  "Personal Instagram": {
-    path: "https://www.instagram.com/im_just.a.me",
-    style: ""
-  },
-  "Tattoo Instagram": {
-    path: "https://www.instagram.com/tat.tohzt",
-    style: ""
-  },
-  "Personal YouTube": {
-    path: "https://www.youtube.com/c/godsautobiography",
-    style: ""
-  },
-  "LetsClone YouTube": {
-    path: "https://www.youtube.com/c/letsclone",
-    style: ""
-  },
+	'Personal Instagram': {
+		path: 'https://www.instagram.com/im_just.a.me',
+		style: '',
+	},
+	'Tattoo Instagram': {
+		path: 'https://www.instagram.com/tat.tohzt',
+		style: '',
+	},
+	'Personal YouTube': {
+		path: 'https://www.youtube.com/c/godsautobiography',
+		style: '',
+	},
+	'LetsClone YouTube': {
+		path: 'https://www.youtube.com/c/letsclone',
+		style: '',
+	},
+	Back: {
+		path: '/',
+		style: '',
+	},
 }
 
 const displaySocials = () => {
-  return Object.keys(socials).map((page, index) => {
-    const style = socials[page]?.style
-    return (
-      <SocialButtons
-        key={index}
-        path={socials[page]?.path}
-        label={page}
-        style={style}
-      />
-    )
-  })
+	return Object.keys(socials).map((page, index) => {
+		const style =
+			socials[page]?.style +
+			(index % 2 === 0 ? ' offset-left' : ' offset-right')
+		return (
+			<SocialButtons
+				key={index}
+				path={socials[page]?.path}
+				label={page}
+				style={style}
+			/>
+		)
+	})
 }
 
 const SocialsPage = () => {
-  return (true &&
-    <>
-      <Header />
+	return (
+		true && (
+			<>
+				<main className='bg-gradient-to-t from-primary-light to-primary-dark'>
+					<Header />
 
-      <div className="flex screen flex-col items-center justify-between bg-gradient-to-b from-primary-dark to-primary-light">
-        <div className="header-margin"></div>
-        <div
-          className="
-            flex 
-            overflow-hidden
-            max-h-[60vh]
-            w-[90vw]
-            p-4 mt-[25vh]
-            bg-secondary-dark 
-            border-4 border-white 
-            rounded-2xl 
-            text-white text-center
-          ">
-          <div 
-            className="
-              bg-secondary-light 
-              flex-col -center 
-              gap-2
-              h-full w-full 
-              overflow-auto 
-              rounded-lg p-2
-              pb-8
-            ">
-            <h1 className="mb-8">Socials</h1>
-            {displaySocials()}
-          </div>
-        </div>
-        <div className="footer-margin"></div>
-      </div>
+					<div className='screen -center flex-col justify-start'>
+						<div className='w-full flex-col gap-4 overflow-y-auto pt-[55vw] sm:pt-[15vh]'>
+							<h1 className='mb-8 text-center font-mono text-2xl font-extrabold text-white'>
+								Socials
+							</h1>
+							{displaySocials()}
+						</div>
+					</div>
 
-      <Footer goBack={true} signIn={false} signOut={false} />
-    </>
-  );
-};
+					<Footer goBack={false} signIn={false} signOut={false} />
+				</main>
+			</>
+		)
+	)
+}
 
 export default SocialsPage
-
