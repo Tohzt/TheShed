7b02b00922192d5d6247a156eb6fa938a96b4f3f
Tohzt <steven.g.french@live.com>
2025-07-03 13:19:07 -0600

    updated socials page

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from 'react'
+import {useRouter} from 'next/router'
 
 interface Props {
-  style?: string;
-  path?: string;
-  label?: string;
+	style?: string
+	path?: string
+	label?: string
 }
 
 const SocialButtons: React.FC<Props> = (props) => {
-  const style = "social-button " + props?.style;
-  const [hover, setHover] = React.useState(false);
-  const [hoverStyle, setHoverStyle] = React.useState("");
+	const style = 'page-button ' + props?.style
+	const [hover, setHover] = React.useState(false)
+	const [hoverStyle, setHoverStyle] = React.useState('')
 
-  React.useEffect(() => {
-    if (hover) setHoverStyle(" button-pressed");
-    else setHoverStyle(" button-released");
-  }, [hover, hoverStyle]);
+	React.useEffect(() => {
+		if (hover) setHoverStyle(' button-pressed')
+		else setHoverStyle(' button-released')
+	}, [hover, hoverStyle])
 
-  const router = useRouter();
+	const router = useRouter()
 
-  const handleClick = () => {
-    void router.push(props?.path);
-  };
+	const handleClick = () => {
+		void router.push(props?.path)
+	}
 
-  return (
-    <div
-      className={style + hoverStyle}
-      onClick={handleClick}
-      onTouchStart={() => { setHover(true) }}
-      onTouchEnd={() => { setHover(false) }}
-    >
-      <div className="w-[80%]">
-        {props?.label}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div
+			className={style + hoverStyle}
+			onClick={handleClick}
+			onTouchStart={() => {
+				setHover(true)
+			}}
+			onTouchEnd={() => {
+				setHover(false)
+			}}
+		>
+			<div className='w-[80%]'>{props?.label}</div>
+		</div>
+	)
+}
 
-export default SocialButtons;
-
-
+export default SocialButtons
